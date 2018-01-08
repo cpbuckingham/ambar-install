8dc78863a3521a96cbc11cbe33059f5a935841bd
cpbuckingham <cameron.p.buckingham@gmail.com>
2018-01-08 16:05:28 -0600

    startup scripts that work for me

--- ambar.py
+++ ambar.py
@@ -10,13 +10,13 @@ import socket
 import time
 import re
 
-AMBAR_LOGO = """ 
+AMBAR_LOGO = """
 
-______           ____     ______  ____       
-/\  _  \  /'\_/`\/\  _`\  /\  _  \/\  _`\    
-\ \ \L\ \/\      \ \ \L\ \\ \ \L\ \ \ \L\ \  
- \ \  __ \ \ \__\ \ \  _ <'\ \  __ \ \ ,  /   
-  \ \ \/\ \ \ \_/\ \ \ \L\ \\ \ \/\ \ \ \\ \  
+______           ____     ______  ____
+/\  _  \  /'\_/`\/\  _`\  /\  _  \/\  _`\
+\ \ \L\ \/\      \ \ \L\ \\ \ \L\ \ \ \L\ \
+ \ \  __ \ \ \__\ \ \  _ <'\ \  __ \ \ ,  /
+  \ \ \/\ \ \ \_/\ \ \ \L\ \\ \ \/\ \ \ \\ \
    \ \_\ \_\ \_\\ \_\ \____/ \ \_\ \_\ \_\ \_\\
     \/_/\/_/\/_/ \/_/\/___/   \/_/\/_/\/_/\/ /
 
@@ -98,34 +98,9 @@ def setOsConstants():
     sysConfig = None
     rcLocal = None
 
-    with open('/etc/sysctl.conf', 'r') as sysConfigFile:
-        sysConfig = sysConfigFile.read()
-    
-    with open('/etc/sysctl.conf', 'a') as sysConfigFile:
-        writeOsConstantIfNotExists(sysConfig, sysConfigFile, 'vm.max_map_count', '262144')
-        writeOsConstantIfNotExists(sysConfig, sysConfigFile, 'net.ipv4.ip_local_port_range', '"15000 61000"')
-        writeOsConstantIfNotExists(sysConfig, sysConfigFile, 'net.ipv4.tcp_fin_timeout', '30')
-        writeOsConstantIfNotExists(sysConfig, sysConfigFile, 'net.core.somaxconn', '1024')
-        writeOsConstantIfNotExists(sysConfig, sysConfigFile, 'net.core.netdev_max_backlog', '2000')
-        writeOsConstantIfNotExists(sysConfig, sysConfigFile, 'net.ipv4.tcp_max_syn_backlog', '2048')        
-        writeOsConstantIfNotExists(sysConfig, sysConfigFile, 'vm.overcommit_memory', '1')      
-
-    with open('/etc/rc.local', 'r') as rcLocalConfigFile:
-        rcLocal = rcLocalConfigFile.read()
-        
-    with open('/etc/rc.local', 'a') as rcLocalConfigFile:
-        writeRcLocalDirectiveIfNotExist(rcLocal, rcLocalConfigFile, 'echo never > /sys/kernel/mm/transparent_hugepage/enabled')      
-
 def setRunTimeOsConstants():
-    runShellCommandStrict('sysctl -w vm.max_map_count=262144')
-    runShellCommandStrict('sysctl -w net.ipv4.ip_local_port_range="15000 61000"')
-    runShellCommandStrict('sysctl -w net.ipv4.tcp_fin_timeout=30')
-    runShellCommandStrict('sysctl -w net.core.somaxconn=1024')
-    runShellCommandStrict('sysctl -w net.core.netdev_max_backlog=2000')
-    runShellCommandStrict('sysctl -w net.ipv4.tcp_max_syn_backlog=2048')
-    runShellCommandStrict('sysctl -w vm.overcommit_memory=1')
-    runShellCommandStrict('echo never > /sys/kernel/mm/transparent_hugepage/enabled')
-        
+    print("makes sense")
+
 def pullImages(configuration):
     dockerRepo = configuration['dockerRepo']
 
@@ -133,29 +108,29 @@ def pullImages(configuration):
     runShellCommandStrict("docker pull {0}/ambar-pipeline:latest".format(dockerRepo))
     runShellCommandStrict("docker-compose -f {0}/docker-compose.yml pull".format(PATH))
 
-def generateEnvFile(configuration):  
+def generateEnvFile(configuration):
     envFileLines = []
 
     if os.path.exists('{0}/.env'.format(PATH)):
         with open('{0}/.env'.format(PATH), 'r') as envFile:
             envFileLines = envFile.readlines()
-    
+
     settings = {}
     for line in envFileLines:
         if '=' in line:
             settings[line.split('=')[0]] = line.split('=')[1]
-    
+
     for key in configuration.keys():
         settings[key]=configuration[key]
 
     newEnvFileLines = []
     for key in settings.keys():
         newEnvFileLines.append('{0}={1}'.format(key, settings[key]))
-    
+
     with open('{0}/.env'.format(PATH), 'w') as envFile:
         envFile.write('\n'.join(newEnvFileLines))
 
-def loadConfigFromFile():   
+def loadConfigFromFile():
     with open('{0}/config.json'.format(PATH), 'r') as configFile:
         config = json.load(configFile)
 
@@ -168,21 +143,21 @@ def loadFromWeb():
 def downloadDockerComposeTemplate():
     runShellCommandStrict('wget -O {0}/docker-compose.yml {1}'.format(PATH, DOCKER_COMPOSE_TEMPLATE_URL))
 
-def install(configuration):                             
+def install(configuration):
     downloadDockerComposeTemplate()
-        
+
     machineAddress = getMachineIpAddress()
 
     print('Assigning {0} IP address to Ambar, type "Y" to confirm or type in another IP address...'.format(machineAddress))
     userInput = input().lower()
     if (userInput == 'y'):
-        configuration['host'] = machineAddress        
+        configuration['host'] = machineAddress
     elif (isValidIpV4Address(userInput)):
         configuration['host'] = userInput
     else:
         print('{0} is not a valid ipv4 address, try again...'.format(userInput))
         return
-    
+
     defaultPort = configuration['port']
     print('Assigning {0} port address to Ambar, type "Y" to confirm or type in another port...'.format(defaultPort))
     userInput = input().lower()
@@ -203,23 +178,23 @@ def install(configuration):
     setOsConstants()
     print('Ambar installed successfully! Run `sudo ./ambar.py start` to start Ambar')
 
-def start(configuration):    
-    setRunTimeOsConstants()    
+def start(configuration):
+    setRunTimeOsConstants()
     generateEnvFile(configuration)
     runShellCommandStrict('docker-compose -f {0}/docker-compose.yml -p ambar up -d'.format(PATH))
     print('Ambar is on {0}://{1}:{2}'.format(configuration['protocol'], configuration['host'], configuration['port']))
 
 def stop(configuration):
     dockerRepo = configuration['dockerRepo']
-    print('Stopping Ambar...')               
-    runShellCommand('docker rm -f $(docker ps -a -q --filter ancestor="{0}/ambar-crawler" --format="{{{{.ID}}}}")'.format(dockerRepo)) 
+    print('Stopping Ambar...')
+    runShellCommand('docker rm -f $(docker ps -a -q --filter ancestor="{0}/ambar-crawler" --format="{{{{.ID}}}}")'.format(dockerRepo))
     print('Crawlers containers removed')
     runShellCommand('docker rm -f $(docker ps -a -q --filter ancestor="{0}/ambar-pipeline" --format="{{{{.ID}}}}")'.format(dockerRepo))
     print('Pipeline containers removed')
     runShellCommand('docker-compose -f {0}/docker-compose.yml -p ambar down'.format(PATH))
-    print('Ambar is stopped')        
+    print('Ambar is stopped')
 
-def update(configuration):    
+def update(configuration):
     stop(configuration)
     downloadDockerComposeTemplate()
     generateEnvFile(configuration)
@@ -234,7 +209,7 @@ def reset(configuration):
     dataPath = configuration['dataPath']
     print('All data from Ambar will be removed ({0}). Are you sure? (y/n)'.format(dataPath))
     choice = input().lower()
-    
+
     if (choice != 'y'):
         return
 
@@ -246,14 +221,14 @@ def uninstall(configuration):
     dataPath = configuration['dataPath']
     print('Ambar will be uninstalled and all data will be removed ({0}). Are you sure? (y/n)'.format(dataPath))
     choice = input().lower()
-    
+
     if (choice != 'y'):
         return
-    
+
     stop(configuration)
     runShellCommand('rm -rf {0}'.format(dataPath))
     runShellCommand('rm -f ./config.json')
-    runShellCommand('rm -f ./docker-compose.yml')  
+    runShellCommand('rm -f ./docker-compose.yml')
     print('Thank you for your interest in our product, you can send your feedback to hello@ambar.cloud. To remove the installer run `rm -f ambar.py`.')
 
 print(AMBAR_LOGO)
@@ -276,9 +251,9 @@ if (args.action == RESTART):
 
 if (args.action == INSTALL):
     if (args.useLocalConfig == True):
-        configuration = loadConfigFromFile()    
+        configuration = loadConfigFromFile()
     else:
-        configuration = loadFromWeb()        
+        configuration = loadFromWeb()
 
     install(configuration)
     exit(0)
@@ -297,5 +272,3 @@ if (args.action == UNINSTALL):
     configuration = loadConfigFromFile()
     uninstall(configuration)
     exit(0)
-
-
